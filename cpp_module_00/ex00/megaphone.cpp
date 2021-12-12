a789c744d9679f3f234767f3464064c6aa219a11
anolivei <anolivei@student.42sp.org.br>
2021-12-11 23:34:27 -0300

    m00 ex00

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -1,12 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 16:42:54 by anolivei          #+#    #+#             */
+/*   Updated: 2021/12/11 23:05:11 by anolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include <cstring>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	char	buff[512];
-
-	std::cout << "Hello world!" << std::endl;
-	std::cout << "Input a word: ";
-	std::cin >> buff;
-	std::cout << "You entered: [" << buff << "]" << std::endl;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (size_t j = 0; j < strlen(argv[i]); j++)
+			argv[i][j] = std::toupper(argv[i][j]);
+		std::cout << argv[i] << " ";
+	}
+	std::cout << std::endl; 
 	return (0);
 }
