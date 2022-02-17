9b3b27a8c741ef0b72040c9df68e4bff00ba89ce
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2022-02-17 00:06:09 -0300

    new test in main m5 ex1

--- cpp_module_05/ex01/main.cpp
+++ cpp_module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 23:29:00 by anolivei          #+#    #+#             */
-/*   Updated: 2022/02/16 22:19:18 by anolivei         ###   ########.fr       */
+/*   Updated: 2022/02/17 00:03:38 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,7 @@ int main(void)
 	std::cout << vasilia << std::endl;
 
 	vasilia.sign_form(form42);
+	std::cout << form42 << std::endl;
 
 	return (0);
 }
