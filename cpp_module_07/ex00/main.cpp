32b11caccf7dde46ffac8bb66baf59af831f2b6a
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2023-03-06 22:54:03 -0300

    ex00 finished

--- cpp_module_07/ex00/main.cpp
+++ cpp_module_07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 22:36:14 by anolivei          #+#    #+#             */
-/*   Updated: 2023/03/06 22:46:24 by anolivei         ###   ########.fr       */
+/*   Updated: 2023/03/06 22:53:36 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int main( void ) {
 	swap(char1, char2);
 	std::cout << "after swap:" << std::endl;
 	std::cout << "char1 = " << char1 << ", char2 = " << char2 << std::endl;
-	std::cout << "min(char1, char2) = " << ::min( char1, char2 ) << std::endl;
-	std::cout << "max(char1, char2) = " << ::max( char1, char2 ) << std::endl;
+	std::cout << "min(char1, char2) = " << ::min(char1, char2) << std::endl;
+	std::cout << "max(char1, char2) = " << ::max(char1, char2) << std::endl;
 
 	std::cout << "\n---- string ----\n" << std::endl;
 	std::string str1 = "Amon";
@@ -34,8 +34,8 @@ int main( void ) {
 	swap(str1, str2);
 	std::cout << "after swap:" << std::endl;
 	std::cout << "str1 = " << str1 << ", str2 = " << str2 << std::endl;
-	std::cout << "min(str1, str2) = " << ::min( str1, str2 ) << std::endl;
-	std::cout << "max(str1, str2) = " << ::max( str1, str2 ) << std::endl;
+	std::cout << "min(str1, str2) = " << ::min(str1, str2) << std::endl;
+	std::cout << "max(str1, str2) = " << ::max(str1, str2) << std::endl;
 
 	std::cout << "\n---- int ----\n" << std::endl;
 	int int1 = 42;
@@ -45,8 +45,8 @@ int main( void ) {
 	swap( int1, int2 );
 	std::cout << "after swap:" << std::endl;
 	std::cout << "int1 = " << int1 << ", int2 = " << int2 << std::endl;
-	std::cout << "min(int1, int2) = " << ::min( int1, int2 ) << std::endl;
-	std::cout << "max(int1, int2) = " << ::max( int1, int2 ) << std::endl;
+	std::cout << "min(int1, int2) = " << ::min(int1, int2) << std::endl;
+	std::cout << "max(int1, int2) = " << ::max(int1, int2) << std::endl;
 
 	std::cout << "\n--- float ----\n" << std::endl;
 	float float1 = 4.2;
@@ -56,6 +56,6 @@ int main( void ) {
 	swap(float1, float2);
 	std::cout << "after swap:" << std::endl;
 	std::cout << "float1 = " << float1 << ", float2 = " << float2 << std::endl;
-	std::cout << "min(float1, float2) = " << ::min( float1, float2 ) << std::endl;
-	std::cout << "max(float1, float2) = " << ::max( float1, float2 ) << std::endl;
+	std::cout << "min(float1, float2) = " << ::min(float1, float2) << std::endl;
+	std::cout << "max(float1, float2) = " << ::max(float1, float2) << std::endl;
 }
