04ac59237adf6dc37657e1a8f6a706015622045b
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2023-03-10 23:03:39 -0300

    ex02 done

---
+++ cpp_module_08/ex02/MutantStack.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 21:13:34 by anolivei          #+#    #+#             */
+/*   Updated: 2023/03/10 23:02:47 by anolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUANTSTACK_HPP
+
+#include <stack>
+#include <deque>
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack: public std::stack<T, Container>
+{
+	public:
+		MutantStack(void): std::stack<T, Container>()
+		{
+			return ;
+		};
+		MutantStack(const MutantStack& obj ): std::stack<T, Container>(obj)
+		{
+			return ;
+		};
+		virtual ~MutantStack(void)
+		{
+			return ;
+		};
+
+		typedef typename Container::iterator iterator;
+		iterator	begin(void)
+		{
+			return (this->c.begin());
+		};
+		iterator	end(void)
+		{
+			return (this->c.end());
+		};
+
+		MutantStack& operator=(const MutantStack& obj)
+		{
+			if (this != &obj)
+				std::stack<T, Container>::operator=(obj);
+			return (*this);
+		};
+};
+
+#endif
---
+++ cpp_module_08/ex02/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 21:13:26 by anolivei          #+#    #+#             */
+/*   Updated: 2023/03/10 23:01:35 by anolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	{
+		std::cout << "\n---- Test with MutantStack (from subject) ----\n"<< std::endl;
+		MutantStack<int> mutantStack;
+		mutantStack.push(5);
+		mutantStack.push(17);
+		std::cout << mutantStack.top() << std::endl;
+		mutantStack.pop();
+		std::cout << mutantStack.size() << std::endl;
+		mutantStack.push(3);
+		mutantStack.push(5);
+		mutantStack.push(737);
+		mutantStack.push(0);
+		MutantStack<int>::iterator it = mutantStack.begin();
+		MutantStack<int>::iterator ite = mutantStack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mutantStack);
+	}
+	{
+		std::cout << "\n---- Test with Vector ----\n"<< std::endl;
+		std::vector<int> vectorVerify;
+		vectorVerify.push_back(5);
+		vectorVerify.push_back(17);
+		std::cout << *(vectorVerify.end() - 1) << std::endl;
+		vectorVerify.pop_back();
+		std::cout << vectorVerify.size() << std::endl;
+		vectorVerify.push_back(3);
+		vectorVerify.push_back(5);
+		vectorVerify.push_back(737);
+		vectorVerify.push_back(0);
+		std::vector<int>::iterator it = vectorVerify.begin();
+		std::vector<int>::iterator ite = vectorVerify.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int, std::vector<int> > s(vectorVerify);
+	}
+	return (0);
+}
